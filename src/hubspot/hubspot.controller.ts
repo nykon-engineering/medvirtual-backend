a5592ebcabacaa8e8f9fdeacff588a8e08d435d1
pauloismelo <seu-email@example.com>
2025-07-16 16:14:02 -0300

    feat: getCandidates route created

--- src/hubspot/hubspot.controller.ts
+++ src/hubspot/hubspot.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, Get } from '@nestjs/common';
+import { HubspotService } from './hubspot.service';
+import { ApiBearerAuth, ApiHeader, ApiOperation } from '@nestjs/swagger';
 
 @Controller('hubspot')
-export class HubspotController {}
+export class HubspotController {
+
+    @Inject()
+    private readonly hubspotService: HubspotService;
+
+
+    //public route for while
+    @ApiOperation({ summary: 'Get candidates from HubSpot on the FOR STAFFING stage' })
+    @Get('candidates')
+    async getCandidates() {
+        return this.hubspotService.getCandidates();
+    }
+
+
+}
+
+
--- src/hubspot/hubspot.service.ts
+++ src/hubspot/hubspot.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Client } from '@hubspot/api-client'
+import { FilterOperatorEnum } from '@hubspot/api-client/lib/codegen/crm/objects';
 
 @Injectable()
 export class HubspotService {
@@ -7,16 +8,76 @@ export class HubspotService {
     private hubspotClient: Client;
 
     constructor() {
-        this.hubspotClient = new Client({ apiKey: process.env.HUBSPOT_ACCESS_TOKEN });
+        this.hubspotClient = new Client({ accessToken: process.env.HUBSPOT_ACCESS_TOKEN });
     }
 
-    async getContacts() {
-        try {
-            const response = await this.hubspotClient.crm.contacts.getAll();
+    async getCandidates() {
+        try{
+            const virtualAssistant = "p20630393_Virtual_Assistant";
+            const fieldtoSearch = "hs_pipeline";
+            const valuetoSearch = '155006239';
+            const properties = ['name', 'id', 'agent_status', 'status', 'stage', 'country', 'date_of_birth', 'email', 'resume_link', 'hs_pipeline', 'hs_pipeline_stage']
+
+            const response = await this.hubspotClient.crm.objects.searchApi.doSearch(virtualAssistant,{
+                filterGroups: [
+                    {
+                        filters: [
+                            {
+                                propertyName: fieldtoSearch,
+                                operator: FilterOperatorEnum.Eq,
+                                value: valuetoSearch,
+                            },
+                            {
+                                propertyName: 'hs_pipeline_stage',
+                                operator: FilterOperatorEnum.Eq,
+                                value: '261075105',
+                            }
+                        ]
+                    }
+                ],
+                properties: properties
+            })
             return response;
-        } catch (error) {
-            console.error('Error fetching contacts from HubSpot:', error);
+        }catch (error) {
+            console.error('Error fetching candidates from HubSpot:', error);
             throw error;
         }
     }
+
+    
+
+
 }
+
+
+/* types found on hubspot | I need to checkout if this status there associations with the stage from hubspot
+=> Create a function to get candidates in one specific stage
+Active in Ops
+
+Failed Recruitment Screening
+
+Pending Start
+
+For Pairing
+
+Resigned
+
+Withdrawn Application (Pre-Ops)
+
+Terminated (Pre-Ops)
+
+Terminated in Ops
+
+Agent Reliability
+
+Agent Performance
+
+Agent Skills Mismatch
+
+Client Change of Business
+
+Client Non-Payment
+
+Client Out of Budget
+
+*/
