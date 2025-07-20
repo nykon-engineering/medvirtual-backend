b0f254e96527e30174aa90c69657533549c47d4d
pauloismelo <seu-email@example.com>
2025-07-20 08:37:33 -0300

    debugging without Dtos

--- src/hubspot/hubspot.service.ts
+++ src/hubspot/hubspot.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { Client } from '@hubspot/api-client'
 import { FilterOperatorEnum } from '@hubspot/api-client/lib/codegen/crm/objects';
-import { GetCandidatesDto } from './dto/get-candidates.dto';
+
 
 @Injectable()
 export class HubspotService {
@@ -12,7 +12,7 @@ export class HubspotService {
         this.hubspotClient = new Client({ accessToken: process.env.HUBSPOT_ACCESS_TOKEN });
     }
 
-    async getCandidates(data: GetCandidatesDto): Promise<any> {
+    async getCandidates(data: any): Promise<any> {
         if (!data.virtualAssistant) throw new BadRequestException('Virtual Assistant identifier is required');
         try{
             const response = await this.hubspotClient.crm.objects.searchApi.doSearch(data.virtualAssistant,{
