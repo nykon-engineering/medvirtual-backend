61aa19a92e6d9150b8db18d31c7c9637f201af41
pauloismelo <seu-email@example.com>
2025-07-20 09:11:02 -0300

    including hubspot DTOs on the service

--- src/hubspot/hubspot.service.ts
+++ src/hubspot/hubspot.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { Client } from '@hubspot/api-client'
 import { FilterOperatorEnum } from '@hubspot/api-client/lib/codegen/crm/objects';
+import { GetCandidatesDto } from './dto/get-candidates.dto';
 
 
 @Injectable()
@@ -9,11 +10,10 @@ export class HubspotService {
     private hubspotClient: Client;
 
     constructor() {
-        console.log('Hubspot Access Token:', process.env.HUBSPOT_ACCESS_TOKEN)
         this.hubspotClient = new Client({ accessToken: process.env.HUBSPOT_ACCESS_TOKEN });
     }
 
-    async getCandidates(data: any): Promise<any> {
+    async getCandidates(data: GetCandidatesDto): Promise<any> {
         if (!data.virtualAssistant) throw new BadRequestException('Virtual Assistant identifier is required');
         try{
             const response = await this.hubspotClient.crm.objects.searchApi.doSearch(data.virtualAssistant,{
