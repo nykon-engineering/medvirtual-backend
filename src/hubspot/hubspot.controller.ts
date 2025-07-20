21f976939c03de375e00d2d4ea6d59ca76a137fc
pauloismelo <seu-email@example.com>
2025-07-20 08:42:29 -0300

    debugging without Dtos

--- src/hubspot/hubspot.controller.ts
+++ src/hubspot/hubspot.controller.ts
@@ -3,7 +3,6 @@ import { Controller, Inject, Get, Body, Post, HttpCode, UseGuards } from '@nestj
 import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { HubspotService } from './hubspot.service';
-import { GetCandidatesDto } from './dto/get-candidates.dto';
 
 import { AuthGuard } from '../auth/auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
@@ -21,12 +20,11 @@ export class HubspotController {
     @Roles('user', 'admin', 'SuperAdmin')
     @HttpCode(200)
     @ApiOperation({ summary: 'Get candidates from HubSpot with dynamic filters' })
-    @ApiBody({ type: GetCandidatesDto })
     @ApiResponse({ status: 200, description: 'Returns candidates below dynamic filters' })
     @ApiResponse({ status: 400, description: 'Data is required' })
     @ApiResponse({ status: 400, description: 'Virtual Assistant identifier is required' })
     @ApiResponse({ status: 400, description: 'Error fetching candidates' })
-    async getCandidates(@Body() data: GetCandidatesDto) {
+    async getCandidates(@Body() data: any) {
         return this.hubspotService.getCandidates(data);
     }
 }
