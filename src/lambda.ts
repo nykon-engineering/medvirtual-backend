27f0c69c62fd3e7e8773bee3e82a730c00b018c0
pauloismelo <seu-email@example.com>
2025-08-01 18:34:33 -0300

    add staging for deployment

--- src/lambda.ts
+++ src/lambda.ts
@@ -16,7 +16,7 @@ async function bootstrapServer(): Promise<any> {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   
   await app.enableCors({
-    origin: ['https://staging.medvirtual.ai', 'http://localhost:3001', , 'http://localhost:3000'], 
+    origin: ['https://app.medvirtual.ai','https://staging.medvirtual.ai','http://localhost:3001','http://localhost:3000'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true
   });
