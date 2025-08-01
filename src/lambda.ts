a62c2757062eff8185693d2ccbc90019d6c115ee
pauloismelo <seu-email@example.com>
2025-08-01 14:03:50 -0300

    configurated cors for medvirtual.ai

--- src/lambda.ts
+++ src/lambda.ts
@@ -14,7 +14,12 @@ async function bootstrapServer(): Promise<any> {
   const expressApp = express();
   expressApp.use(express.json());
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  await app.enableCors();
+  
+  await app.enableCors({
+    origin: 'https://medvirtual.ai', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true
+  });
 
   await app.useGlobalPipes(
     new ValidationPipe({
