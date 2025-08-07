a967af9b8352c733a0ff1d6a7dc00be499f42480
pauloismelo <seu-email@example.com>
2025-08-07 18:17:57 -0300

    cron module with tests

--- src/cron/cron.service.spec.ts
+++ src/cron/cron.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CronService } from './cron.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('CronService', () => {
   let service: CronService;
 
+  const prismaServiceMock = {
+    candidate: {
+      findMany: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CronService],
+      providers: [CronService,
+        {provide: PrismaService, useValue: prismaServiceMock}
+      ],
     }).compile();
 
     service = module.get<CronService>(CronService);
