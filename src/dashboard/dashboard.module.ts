c608fb9f69d9531f69b45e2aee3124e23a7d88ab
pauloismelo <seu-email@example.com>
2025-08-12 12:55:59 -0300

    new handler to dashboard

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -3,10 +3,11 @@ import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { HandlerOrganization } from './handlers/organization';
+import { HandlerClient } from './handlers/client';
 
 @Module({
   controllers: [DashboardController],
-  providers: [DashboardService, HandlerOrganization],
+  providers: [DashboardService, HandlerOrganization, HandlerClient],
   imports: [PrismaModule]
 })
 export class DashboardModule {}
