69984de220ab8d71b62dd57ab6e2e7fbb8c46d12
pauloismelo <seu-email@example.com>
2025-08-20 19:40:34 -0300

    feat: include IsOptional

--- src/hire-request/dto/reassign-hire-request.dto.ts
+++ src/hire-request/dto/reassign-hire-request.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 export class reassignDTO {
     @ApiProperty({ example: 'user_id', description: 'The ID of the user to whom the hire request is reassigned', required: true, type: String })
     @IsString()
+    @IsOptional()
     user_id: string;
 }
