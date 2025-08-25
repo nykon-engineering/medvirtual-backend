2d278a2fb82bc0db9147505c1a0855914840de86
pauloismelo <seu-email@example.com>
2025-08-24 21:54:36 -0300

    add sync with hubspot on the change-status

--- src/common/utils/hubspot-updateMany.util.ts
+++ src/common/utils/hubspot-updateMany.util.ts
@@ -14,7 +14,7 @@ const hubspotClient = axios.create({
 
 
 export async function hubspotUpdateMany(candidates, pipelineStatus) {
-  return true; // Temporarily returning true to avoid breaking changes
+  //return true; // Temporarily returning true to avoid breaking changes
 
   try {
     const inputs = candidates.map(c => ({
