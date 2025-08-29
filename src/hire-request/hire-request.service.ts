3702587ab7d975d500a60a66d889fa0005d4e767
pauloismelo <seu-email@example.com>
2025-08-29 09:44:24 -0300

    fix: change requirement on the findOne hireRequest

--- src/hire-request/hire-request.service.ts
+++ src/hire-request/hire-request.service.ts
@@ -205,7 +205,7 @@ export class HireRequestService {
     const hireRequest = await this.prisma.hireRequest.findUnique({
       where: {
         id: id,
-        organization: { id : user.organization_id,}
+        organization: user.role.includes('organization') ?  { id: user.organization_id } : undefined,
       },
       include: {
         skills: true,
