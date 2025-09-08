5aca2fe4e18b8988d510cd5aafe9abe7ff9b7743
pauloismelo <seu-email@example.com>
2025-09-08 16:37:21 -0300

    including decision_made on the getPanels HireREquest

--- src/hire-request/hire-request.service.ts
+++ src/hire-request/hire-request.service.ts
@@ -1053,6 +1053,10 @@ export class HireRequestService {
             status: 'decision_pending',
             readable: true,
           },
+          { 
+            status: 'decision_made',
+            readable: true,
+          },
           { status: 'interview_completed' },
           { status: 'interview_scheduled' }
         ]
