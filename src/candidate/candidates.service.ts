2d9baa93027fd82d6d52013f8806b3b1d2ad64e6
pauloismelo <seu-email@example.com>
2025-09-11 14:37:21 -0300

    change rule to candidate module

--- src/candidate/candidates.service.ts
+++ src/candidate/candidates.service.ts
@@ -45,8 +45,13 @@ export class CandidatesService {
 
     const skip =(page - 1) * perPage;
     const take = perPage;
+
+    
+    
+    if (!user || user.role.includes("organization") && !user.organization_id) 
+      throw new BadRequestException('The current user doent have an organization_id');
+
     const {organization_id} = user;
-    if (!organization_id) throw new BadRequestException('The current user doent have an organization_id');
 
     const hourly_from = monthly_compensation_from ? Number(monthly_compensation_from) / (Number(process.env.CANDIDATE_HOUR_PER_MONTH) * Number(process.env.CANDIDATE_PERCENT)) : undefined; 
     const hourly_to = monthly_compensation_to ? Number(monthly_compensation_to) / (Number(process.env.CANDIDATE_HOUR_PER_MONTH) * Number(process.env.CANDIDATE_PERCENT)) : undefined; 
@@ -250,7 +255,7 @@ export class CandidatesService {
       
       const interviewRequestTickets = await this.prisma.ticket.findMany({
         where: {
-          organization: { is: { id: organization_id} },
+          organization: { is: { id: organization_id || undefined} },
           type: 'interview',
           status: {
             in: ['new', 'in_progress']
