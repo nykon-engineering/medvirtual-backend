c022974b10cbc9c05332b96982a46a6a200236ae
pauloismelo <seu-email@example.com>
2025-09-11 15:08:50 -0300

    adding pipeline_status on the return

--- src/hire-request/hire-request.service.ts
+++ src/hire-request/hire-request.service.ts
@@ -1125,6 +1125,7 @@ export class HireRequestService {
                 educations: true,
                 experiences: true,
                 skills:true,
+                pipeline_status: true
               },
             },
           },
