854a6ac82e07ecc740adea9bd82b9ecbc3971df0
pauloismelo <seu-email@example.com>
2025-09-29 19:31:07 -0300

    All hireRequest being created as 'new'

--- src/hire-request/hire-request.service.ts
+++ src/hire-request/hire-request.service.ts
@@ -91,7 +91,9 @@ export class HireRequestService {
     const hireRequest = {
       ...hireRequestData,
       organization: user.role.includes('organization') ?  {connect: {id: user.organization_id || undefined}} : { connect : { id: client_id } },
-      status: organizationSQL.organization_role !== OrganizationRole.client ? 'pending_signature' as HireRequestStatus : 'new' as HireRequestStatus,
+      //removed the status pending signature asked by Pauli: https://regenta-company.monday.com/boards/9328303960/pulses/18070949199
+      //status: organizationSQL.organization_role !== OrganizationRole.client ? 'pending_signature' as HireRequestStatus : 'new' as HireRequestStatus,
+      status: HireRequestStatus.new,
       assigned_user: organizationSQL.admin_id ? { connect: { id: organizationSQL.admin_id } } : undefined,
     };
 
