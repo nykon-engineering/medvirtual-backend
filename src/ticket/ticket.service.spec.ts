468f1eacf2cd6fbad4b12b15d402e6675e56dd51
babettsan <babettsan@gmail.com>
2025-10-08 18:33:29 -0300

    extend TicketService tests to validate candidate and staff selection fields

--- src/ticket/ticket.service.spec.ts
+++ src/ticket/ticket.service.spec.ts
@@ -226,6 +226,34 @@ describe('TicketService', () => {
               status: true,
               email: true,
             }
+          },
+          candidate: {
+            select: {
+              id: true,
+              first_name: true,
+              last_name: true,
+              email: true,
+              name: true,
+            }
+          },
+          staff: {
+            select: {
+              id: true,
+              status: true,
+              start_date: true,
+              terminated_date: true,
+              candidate: {
+                select: {
+                  id: true,
+                  first_name: true,
+                  last_name: true,
+                  email: true,
+                  name: true,
+                  specialization: true,
+                  years_of_experience: true,
+                }
+              }
+            }
           }
         }
       });
