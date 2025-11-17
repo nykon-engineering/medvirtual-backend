9e32b9c81653d3091fd8fe1a4f28e71f5a618fc0
pauloismelo <seu-email@example.com>
2025-11-17 14:31:45 -0300

    handling with the pay_range text from hubspot

--- src/hubspot/handlers/ticketPropertyChange.ts
+++ src/hubspot/handlers/ticketPropertyChange.ts
@@ -18,6 +18,22 @@ export class HandlerTicketPropertyChange {
         })
         if(!hr) return;
 
+        if (event.propertyName === 'va_pay_rate_range') {
+            //We receive a text like '120 - 150' and we need to parse it for 2 fields: salary_range_from and salary_range_to
+            const [from, to] = event.propertyValue.split(' - ').map(value => parseFloat(value.trim()));
+            await this.prisma.hireRequest.update({
+                where: {
+                    id: hr.id
+                },
+                data: {
+                    salary_range_from: from,
+                    salary_range_to: to
+                }
+            })
+
+            return true;
+        }
+
         const fieldExists = Object.keys(hrTicketToDbDictionary).includes(event.propertyName);
         if(!fieldExists) return;
 
