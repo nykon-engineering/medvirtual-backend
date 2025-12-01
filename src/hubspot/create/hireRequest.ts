b6ae91dbedef2531df2f4c7268870fe3f845fcd0
pauloismelo <seu-email@example.com>
2025-12-01 14:08:47 -0300

    fix: ensuring both sides can create HR and reflect on hubspot

--- src/hubspot/create/hireRequest.ts
+++ src/hubspot/create/hireRequest.ts
@@ -42,7 +42,7 @@ export class HireRequestCreationService {
           const pay_range= data.salary_range_from && data.salary_range_to 
           ? `${data.salary_range_from} - ${data.salary_range_to}` 
           : '';
-          
+          //console.log("Data arriving on HireRequestCreationService:", data);
           const response = await axios.post(
             "https://api.hubapi.com/crm/v3/objects/tickets",
             {
@@ -64,20 +64,20 @@ export class HireRequestCreationService {
                 language: data.hubspot_language,
                 number_of_vas: data.hubspot_numberVA.toString(),
                 va_pay_rate_range: pay_range.toString(),
-                tasks: data.hubspot_tasks,
+                tasks: data.hubspot_tasks ? data.hubspot_tasks : undefined,
                 n2_monitors_required_: data.hubspot_n2_monitors_required,
-                va_shift_hours: data.hubspot_va_shift_hours,
+                va_shift_hours: data.hubspot_va_shift_hours ? data.hubspot_va_shift_hours : undefined,
                 special_sourcing_needed: data.hubspot_special_sourcing_needed === 'Yes' ? 'true' : 'false',
-                special_requirements: data.hubspot_special_requirements,
-                additional_training_requested: data.hubspot_additional_training_requested,
+                special_requirements: data.hubspot_special_requirements ? data.hubspot_special_requirements : undefined,
+                additional_training_requested: data.hubspot_additional_training_requested ? data.hubspot_additional_training_requested : undefined,
                 pairing_date: data.hubspot_pairing_date ? data.hubspot_pairing_date : undefined, //  milisecnonds in timestamp,
-                pairing_time: data.hubspot_pairing_time,
+                pairing_time: data.hubspot_pairing_time ? data.hubspot_pairing_time : undefined,
 
                 //ticketOwner
                 hubspot_owner_id: data.assign_user_id ? await this.getOwnerId(data.assign_user_id) : undefined,
                 //hire_date__start_of_employment_: PairingDate, 'expected_start_date', => issue form hubspot saying 'Enter a date before ${currentDate}': 
               },
-              associations: [
+              associations: data.organization.hubspot_id ? [
                 {
                   to: { id: data.organization.hubspot_id }, 
                   types: [
@@ -87,7 +87,8 @@ export class HireRequestCreationService {
                     },
                   ],
                 },
-              ],
+              ]
+              : undefined,
             },
             {
               headers: {
