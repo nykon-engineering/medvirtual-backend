22970a2a3fc69dbd7291c338cd2a3e4ddd6272d5
pauloismelo <seu-email@example.com>
2026-02-04 12:15:47 -0300

    adding payload

--- src/lambda-sqs.ts
+++ src/lambda-sqs.ts
@@ -21,6 +21,7 @@ export const handler = async (event: SQSEvent) => {
   for (const record of event.Records) {
     //console.log('Processing record:', record.body);
     const payload = JSON.parse(record.body);
+    console.log('Processing payload:', payload);
     switch (payload.type) {
       case 'CREATE_DEAL_STAFF':
         //Process deal creation
