1f0bbfa6ef9688fe02145479781f0b63a758f422
pauloismelo <pauloisaquecpd@hotmail.com>
2026-08-24 12:07:20 -0300

    fix: solve circular dependency at sqs module

--- src/sqs/sqs.module.ts
+++ src/sqs/sqs.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SqsService } from './sqs.service';
 import { DealsQueueConsumerService } from './deals-queue-consumer.service';
 import { HubspotModule } from '../hubspot/hubspot.module';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [HubspotModule, PrismaModule],
+  imports: [forwardRef(() => HubspotModule), PrismaModule],
   providers: [SqsService, DealsQueueConsumerService],
   exports: [SqsService, DealsQueueConsumerService],
 })
